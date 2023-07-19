9d8ce30db8e85413c5b8044aba54a16eac410589
Drei4 <antipin-lukinf@mail.ru>
2023-07-19 20:13:56 +0300

    matrix complit

--- matrix.py
+++ matrix.py
@@ -4,54 +4,91 @@
 ○ сравнения,
 ○ сложения,
 """
-import random
 
 
-class Matrix2D:
-    def __init__(self):
-        '''
-        Создание матрицы.
-        '''
-        self.matrix = []
-        self.rows = 0
-        self.columns = 0
-        self.name = 'Unnamed'
+class Matrix(object):
+    """Класс Матрица"""
+    def __init__(self, list2D):
+        self.list2D = list2D
+        self.dim_R = len(self.list2D)
+        self.dim_C = len(self.list2D[0])
 
-    def __str__(self):
-        return self.matrix
-
-    def generate(self, rows, columns, verbose=False):
-        '''
-         Возвращает список списков, содержащих сумму индексов матрицы .
-        int, int -> [[(int + int), ...], ...]
-        '''
-        self.rows = rows
-        self.columns = columns
-        self.matrix = [[(row + col + random.randint(1, 10)) for col in range(columns)] for row in range(rows)]
-
-        return self.matrix
-
-    def printme(self, verbose=False):
-        '''
-        Вывод матрицы построчно.
-        '''
-        if verbose:
-            print(f'{self.rows}  {self.columns} matrix')
-        for row in self.matrix:
-            print(row)
+    def __repr__(self):
+        res = ''
+        for x in self.list2D:
+            s = ''
+            for y in x:
+                s += str(y) + '    '
+            res += s.strip() + '\n'
+        return res.strip()
 
     def __add__(self, other):
-        new_rows = self.rows + other.rows
-        new_columns = self.columns + other.columns
+        """Метод складывания матриц"""
+        if self.dim_R == other.dim_R and self.dim_C == other.dim_C:
+            result = []
+            res = []
+            for x in range(self.dim_R):
+                for y in range(self.dim_C):
+                    sum = self.list2D[x][y] + other.list2D[x][y]
+                    res.append(sum)
+                result.append(res)
+                res = []
+            return Matrix(result)
+        else:
+            return 'Размерности не совпадают'
+
+    def __sub__(self, other):
+        """Метод вычитания матриц"""
+        if self.dim_R == other.dim_R and self.dim_C == other.dim_C:
+            res = []
+            result = []
+            for x in range(self.dim_R):
+                for y in range(self.dim_C):
+                    sub = self.list2D[x][y] - other.list2D[x][y]
+                    res.append(sub)
+                result.append(res)
+                res = []
+            return Matrix(result)
+        else:
+            return 'Размерности не совпадают'
+
+    def __mul__(self, other):
+        """Метод умножения матриц"""
+        if isinstance(other, int) or isinstance(other, float):
+            result = [[other * x for x in y] for y in self.list2D]
+            return Matrix(result)
+        elif self.dim_C != other.dim_R:
+            return 'Нельзя перемножить матрицы таких размерностей'
+        else:
+            a = range(self.dim_C)
+            b = range(self.dim_R)
+            c = range(other.dim_C)
+            result = []
+            for i in b:
+                res = []
+                for j in c:
+                    el, m = 0, 0
+                    for k in a:
+                        m = self.list2D[i][k] * other.list2D[k][j]
+                        el += m
+                    res.append(el)
+                result.append(res)
+            return Matrix(result)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
 
 
-m_1 = Matrix2D()
-m_2 = Matrix2D()
-m_1.generate(4, 5, True)
-m_2.generate(4, 5, True)
-m_1.printme()
+m1 = Matrix([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+m2 = Matrix([[3, 1, 3], [5, 2, 8], [6, 8, 4]])
+print(m1)
 print('------------------')
-m_2.printme()
+print(m2)
 print('------------------')
-m = m_1 + m_2
-print(m)
+print(m1 == m2)
+print('------------------')
+print(m1 + m2)
+print('------------------')
+print(m1 * m2)
+
+help(Matrix)
